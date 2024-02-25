ec4ac02c1f41ec8e19119621d32ef5f05324cc64
Jinha Song <jinhasong@sogang.ac.kr>
2024-02-25 12:51:55 +0900

    Update download_models.sh

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -11,7 +11,7 @@ fi
 
 echo "======================================="
 echo "Download start(yolov7-w6 display fault)"
-wget -q ftp://mldisk.sogang.ac.kr/aixcon/display_fault/yolov7-w6.pt -O /workspace/model/weights/yolov7-w6.pt \
+wget -q ftp://mldisk.sogang.ac.kr/aixcon/display_fault/disk3/yolov7-w6.pt -O /workspace/model/weights/yolov7-w6.pt \
 && echo "Download successful(yolov7-w6 display fault)" \
 || echo "\e[31mDownload failed(yolov7-w6 display fault)\e[0m"
 echo "======================================="
